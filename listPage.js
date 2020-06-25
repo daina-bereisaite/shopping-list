ffc40ba18d967c822252afb06966ab7d6502f487
Daina Bereisaite <bereisaite.daina@gmail.com>
2020-06-25 13:37:12 +0300

    Adding alert for empty submit option + close button for list item

--- listPage.js
+++ listPage.js
@@ -1,13 +1,32 @@
 
-window.onload=function(){
+window.onload = function () {
     document.getElementById("addButton").addEventListener("click", newElement, false);
 }
 
 function newElement() {
-    var list = document.getElementById('listItems');
     var itemInput = document.getElementById('itemInput').value;
-    var entry = document.createElement("LI");
-    entry.appendChild(document.createTextNode(itemInput));
+    if (itemInput === '') {
+        alert("Write something");
+        return;
+    }
+
+    var list = document.getElementById('listItems');
+
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    span.onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+
+    var textNode = document.createTextNode(itemInput);
+
+    var entry = document.createElement("li");
+
+    entry.appendChild(textNode);
+    entry.appendChild(span);
+
     list.appendChild(entry);
 }
-
